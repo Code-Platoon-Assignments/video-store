a8af1ad913f4ac7b696e2bd40bd03fc441eb211d
Benjamin Cohen <benjamin@codeplatoon.org>
2023-04-17 13:38:17 -0600

    pt 1 starting point

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css';
+import inventory from './data/inventory.json';
+
+console.log(inventory);
+
+export default function App() {
+  return (
+    <div id="app_root">
+      <header>
+        <h1>Video Store</h1>
+      </header>
+      <main>
+        <h1>Home Page goes here</h1>
+      </main>
+      <footer>© 2023 Video Store</footer>
+    </div>
+  );
+}
